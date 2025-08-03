97a4968abbca41056c125d81407a0eada89c71b4
DzyadzoMM <m.dziadzo@gmail.com>
2025-08-03 13:08:52 +0300

    first

--- src/services/moviesService.ts
+++ src/services/moviesService.ts
@@ -1,18 +1,28 @@
 import axios from "axios";
-import { Movie } from "../types/movie";
+import { Movie, MovieDetails } from "../types/movie";
 
 interface fetchMoviesProps {
   results: Movie[];
 }
 
 export async function fetchMovies (query: string): Promise<Movie[]> {
-
-    const response = await axios.get<fetchMoviesProps>("https://api.themoviedb.org/3/search/movie", {
+  const response = await axios.get<fetchMoviesProps>("https://api.themoviedb.org/3/search/movie", {
     params: { query },
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
+  });
+  return response.data.results;
+}
+
+export async function getMovieDetails(movieId: number): Promise<MovieDetails> {
+  const response = await axios.get<MovieDetails>(
+    `https://api.themoviedb.org/3/movie/${movieId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+      },
     }
-  )
-   return response.data.results;
+  );
+  return response.data;
 }
